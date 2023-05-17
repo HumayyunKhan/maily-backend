78fe0f9fdced4bed5030c98680ad5f1a78572429
Humayyun00 <humayyun@techmania.pk>
2023-05-17 14:11:04 +0500

    setup

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app=express()
 app.use(express.json())
 app.listen(3000)
 app.get('/',(req,res)=>{
-    res.send("WELCOME TO MAILY")
+    res.send("WELCOME TO MAILY")   
+
+    
 })
 
 app.use('/maily',routes)
